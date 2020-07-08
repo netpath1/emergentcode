61a64ffe0729d1c043e6102976b8651f7ea9d24a
netpath1 <pat_hennessy@msn.com>
2020-07-08 14:38:53 -0500

    Changed view to use EditForm and added input validation.

--- Software/Shared/SoftwareSearchItem.cs
+++ Software/Shared/SoftwareSearchItem.cs
@@ -1,10 +1,14 @@
-﻿namespace Software.Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Software.Shared
 {
     public class SoftwareSearchItem
     {
         #region Public Properties
 
         public string Name { get; set; }
+
+        [RegularExpression("^(\\d+\\.)?(\\d+\\.)?(\\d+\\.)?(\\*|\\d+)$", ErrorMessage = "Version filter is invalid.")]
         public string Version { get; set; }
 
         #endregion 
